f4abff90e7224fa2c728485026b8d39b4cbfc778
kumaraswamy <care.xoma@gmail.com>
2026-06-03 03:38:37 +0530

    Serve built app from GitHub Pages root

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: './',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name][extname]',
+      },
+    },
+  },
   plugins: [react()],
 })
